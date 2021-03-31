b65bdcd2837ae226859abae27f196a68a4587f8d
Isaac B. Jessup <ibjessup@gmail.com>
2021-03-30 20:01:57 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net"
 	"os"
@@ -31,12 +32,15 @@ func main() {
 }
 func handle(t1 []string, t2 []string, direction int) {
 	for {
+		fmt.Println("starting listener of type " + t1[0] + " at " + t1[1])
 		l, _ := net.Listen(t1[0], t1[1])
 		for {
 			c1, err := l.Accept()
 			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
 				continue
 			}
+			fmt.Println("starting dialer of type " + t2[0] + " at " + t2[1])
 			switch direction {
 			case 0:
 				go outputOnly(c1, t2)
@@ -51,17 +55,26 @@ func handle(t1 []string, t2 []string, direction int) {
 
 func outputOnly(c1 net.Conn, target []string) {
 	defer c1.Close()
-	c2, _ := net.Dial(target[0], target[1])
+	c2, err := net.Dial(target[0], target[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+	}
 	io.Copy(c2, c1)
 }
 func inputOnly(c1 net.Conn, target []string) {
 	defer c1.Close()
-	c2, _ := net.Dial(target[0], target[1])
+	c2, err := net.Dial(target[0], target[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+	}
 	io.Copy(c1, c2)
 }
 func bidirectional(c1 net.Conn, target []string) {
 	defer c1.Close()
-	c2, _ := net.Dial(target[0], target[1])
+	c2, err := net.Dial(target[0], target[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+	}
 	go io.Copy(c1, c2)
 	io.Copy(c2, c1)
 }
